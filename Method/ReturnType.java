390ded76b50e1227ff4ebf5d0f8520f0501fc0fb
Anshika-Srivastava-9 <srianshika89@gmail.com>
2025-10-08 23:52:03 +0530

    Return Type

---
+++ Method/ReturnType.java
@@ -0,0 +1,14 @@
+package Method;
+
+public class ReturnType {
+    public static int sum(int a,int b) {
+       return a+b; 
+    }
+    public static void main(String[] args) {
+       System.out.println(sum(2,6));
+    }
+}
+
+
+// javac Method\ReturnType.java
+// java Method.ReturnType 
