d5c1c420f95651c60933c5a8d347fd4da2dc59a8
Anshika-Srivastava-9 <srianshika89@gmail.com>
2025-10-08 22:32:12 +0530

    Methods

--- Method/Power.java
+++ Method/Power.java
@@ -5,13 +5,23 @@ import java.util.Scanner;
 public class Power {
     public static void main(String[] args) {
         Scanner sc=new Scanner (System.in);
-        int a=sc.nextInt();
-        int b=sc.nextInt();
-        int pow=1;
-        for (int i = 1; i < b; i++) {
-            pow*=a;
+        // int a=sc.nextInt();
+        // int b=sc.nextInt();
+        // int pow=1;
+        // for (int i = 1; i < b; i++) {
+        //     pow*=a;
             
-        }
-        System.out.println(pow);
+        // }
+        // System.out.println(pow);
+
+
+        System.out.println(Math.pow(4,3));
+        System.out.println(Math.sqrt(5));
+        System.out.println(Math.cbrt(64));
+        System.out.println(Math.min(49,64));
+        System.out.println(Math.max(-32,64));
+        System.out.println(Math.abs(-64));
+        System.out.println(Math.floor(4.64));
+        System.out.println(Math.ceil(64));
     }
 }
