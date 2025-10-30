9af5fb8dcc5af7b14906da96de56930329169125
Anshika-Srivastava-9 <srianshika89@gmail.com>
2025-10-30 22:53:51 +0530

    All divisors of a number

---
+++ Maths/AllDivisors.java
@@ -0,0 +1,21 @@
+package Maths;
+import java.util.Scanner;
+
+public class AllDivisors {
+    public static void main(String[] args) {
+        Scanner  sc=new Scanner (System.in);
+    int n=36;
+    for (int i=1;i<=Math.sqrt(n);i++){
+        if(n%i==0){
+        System.out.print(i+" ");
+        if((n/i)!=i)
+        System.out.print(n/i+" ");
+        
+        }
+        
+    }
+    
+    }
+}
+// javac Maths/AllDivisors.java
+// java Maths.AllDivisors
