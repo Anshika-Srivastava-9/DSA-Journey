2e34b173ead090f75bc9bb519abaf8e15a24dc92
Anshika-Srivastava-9 <srianshika89@gmail.com>
2025-11-01 17:38:41 +0530

    Taking Input and printing output

---
+++ Array/Input_Output.java
@@ -0,0 +1,13 @@
+package Array;
+
+public class Input_Output {
+  public static void main(String[] args) {
+    int arr[]={7,5,3,4,53,65,23,86};
+    System.out.println(arr.length);              // length of Array
+    for (int i = 0; i <= arr.length; i++) {
+     System.out.print(arr[i]+" ");   
+    }
+  }  
+}
+// javac Array/Input_Output.java
+// java Array.Input_Output
