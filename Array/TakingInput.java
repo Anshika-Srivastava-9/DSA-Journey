1aa1b396428ca4e6db54a29895cdb37e95e2ff0a
Anshika-Srivastava-9 <srianshika89@gmail.com>
2025-11-01 17:39:51 +0530

    Taking Input

---
+++ Array/TakingInput.java
@@ -0,0 +1,22 @@
+package Array;
+
+import java.util.Scanner;
+
+public class TakingInput {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner (System.in);
+        System.out.println("Enter The size of Array:");
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for (int i = 0; i <n; i++) {
+            arr[i]=sc.nextInt();
+            
+        }
+        for (int i = 0; i <n; i++) {
+            System.out.print(arr[i]+" ");
+            
+        }
+    }
+}
+// javac Array/TakingInput.java
+// java Array.TakingInput
