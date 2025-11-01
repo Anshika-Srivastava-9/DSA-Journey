2ae08e258736309bb25ab2b07d4d1cf281c9e04a
Anshika-Srivastava-9 <srianshika89@gmail.com>
2025-11-01 17:15:32 +0530

    Initializationn of Array

---
+++ Array/Initialization.java
@@ -0,0 +1,19 @@
+package Array;
+
+public class Initialization {
+   public static void main(String[] args) {
+    int[] arr=new int[7];
+    System.out.println(arr[4]);
+    arr[0]=1;
+    arr[1]=4;
+    arr[2]=3;
+    arr[3]=6;
+    arr[4]=34;
+    arr[5]=15;
+    arr[6]=17;
+    System.out.println(arr[4]);
+   } 
+}
+
+// javac Array/Initialization.java
+// java Array.Initialization
