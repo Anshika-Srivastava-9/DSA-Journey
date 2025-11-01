5e5bc559af5ebe5f2e38748aee33d0df072fd348
Anshika-Srivastava-9 <srianshika89@gmail.com>
2025-11-01 17:11:04 +0530

    Basics of Array

---
+++ Array/Basic.java
@@ -0,0 +1,16 @@
+package Array;
+
+public class Basic {
+    public static void main(String[] args) {
+      int [] x={76,32,43,21,54};
+      int a=10;
+      System.out.println(x[2]);
+      System.out.println(x[0]);
+      System.out.println(x[3]);
+    x[3]=34;
+    System.out.println(x[3]);
+    }
+    
+}
+// javac Array/Basic.java
+// java Array.Basic
