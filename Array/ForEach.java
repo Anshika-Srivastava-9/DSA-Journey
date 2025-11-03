4b67b90ca57e392ea772824a15b12aa7acbfb5fe
Anshika-Srivastava-9 <srianshika89@gmail.com>
2025-11-04 01:27:26 +0530

    Basic Understanding of ForEach Loop

---
+++ Array/ForEach.java
@@ -0,0 +1,18 @@
+package Array;
+
+public class ForEach {
+    public static void main(String[] args) {
+       int arr[]={4,2,9,5,7} ;
+       for(int ele:arr){
+        System.out.println(ele+" ");
+       }
+    
+    for (int ele:arr){
+        ele*=2;
+    }
+    for (int ele : arr) {
+         System.out.println(ele+" ");       // does not make any changes becase it is used to print element and treverse data structure, only
+    }
+              
+    }
+    }
