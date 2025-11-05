690f80c90ec5ff13c66076184af6bf684acf3328
Anshika-Srivastava-9 <srianshika89@gmail.com>
2025-11-05 20:42:52 +0530

    Checking for duplicate element in array

---
+++ Array/Duplicate.java
@@ -0,0 +1,17 @@
+package Array;
+
+public class Duplicate {
+    public static void main(String[] args) {
+        boolean duplicate=false;
+        int []arr={1,2,3,2,45,3,65};
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]==arr[j]){
+                    duplicate=true;
+                    break;
+                }
+            }
+        }
+        return;
+    }
+}
