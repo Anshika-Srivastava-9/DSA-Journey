4d69e804645bb0395885f41d549f6c812b4dfdde
Anshika-Srivastava-9 <srianshika89@gmail.com>
2026-01-27 13:56:43 +0530

    Bubble Sort in decreasing order

---
+++ Sorting/BubbleDec.java
@@ -0,0 +1,28 @@
+public class BubbleDec{
+    public static void print (int arr[]){
+        for(int ele:arr){
+            System.out.print(ele+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+     int arr[]={3,5,2,6,9,112,4,1};
+     int n =arr.length;
+     print(arr);
+     for(int i=0;i<n-1;i++){
+        boolean flag=true;
+        for(int j=0;j<n-1-i;j++){
+           if(arr[j]<arr[j+1]){
+            int temp=arr[j];
+            arr[j]=arr[j+1];
+            arr[j+1]=temp;
+            flag=false;
+           } 
+           
+        }
+        if(flag==true)
+        break;
+             }   
+       print(arr);
+    }
+}
