b2fc41ab278b9a33220b3827a2e6f9ab9e50803d
Anshika-Srivastava-9 <srianshika89@gmail.com>
2026-01-28 19:36:57 +0530

    Selection sort increasing order

---
+++ Sorting/SelectionSort.java
@@ -0,0 +1,26 @@
+public class SelectionSort {
+ public static void print (int arr[] ) {
+   for(int ele:arr){
+    System.out.print(ele+" ");
+   } 
+   System.out.println();
+ }   
+ public static void main(String[] args) {
+    int arr[]={2,4,-1,6,0,1,-4,-3,9};
+    for(int i=0;i<arr.length-1;i++){
+        int min=Integer.MAX_VALUE;
+        int mindx=-1;
+        for(int j=i;j<arr.length-1;j++){
+            if(arr[j]<min){
+                min=arr[j];
+                mindx=j;
+            }
+        }
+              int temp=arr[i];
+                arr[i]=arr[mindx];
+               arr[mindx] =temp;
+    
+    }
+   print(arr); 
+ }
+}
