3fa3f6a5d47d582e7fae377d9b83bedfaca89572
Anshika-Srivastava-9 <srianshika89@gmail.com>
2026-03-15 21:31:18 +0530

    All possible pairs in Array

---
+++ Array/Pairs.java
@@ -0,0 +1,18 @@
+package Array;
+
+public class Pairs {
+  public static void PairsInArray(int arr[])  {
+    for(int i=0;i<arr.length;i++){
+        int current=arr[i];
+        for(int j=i+1;j<arr.length;j++){
+            System.out.print("(" +current+ ","  +arr[j] + ")");
+        }
+    System.out.println();
+    }
+   
+  }
+  public static void main(String[] args) {
+    int arr[]={2,3,5,4,7,8};
+    PairsInArray(arr);
+  }
+}
