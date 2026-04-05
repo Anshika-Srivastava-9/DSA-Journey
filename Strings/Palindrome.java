65ebcb3e013afa7e3584cd81325c2f40c2fb5a04
Anshika-Srivastava-9 <srianshika89@gmail.com>
2026-04-05 15:10:17 +0530

    Checking Palindrome

---
+++ Strings/Palindrome.java
@@ -0,0 +1,22 @@
+package Strings;
+
+public class Palindrome {
+    public static boolean isPalindrome(String str){
+        int n=str.length();
+        int i=0;
+        int j=str.length()-1;
+        while(i<j){
+            if(str.charAt(i)!=str.charAt(j)){
+              return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+
+    }
+    public static void main(String[] args) {
+        String str="racecar";
+        System.out.println(isPalindrome(str));
+    }
+}
