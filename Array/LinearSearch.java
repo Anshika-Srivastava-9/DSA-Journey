cf04291c5983e1dc75506edcc8f9a9ac98385bc3
Anshika-Srivastava-9 <srianshika89@gmail.com>
2026-04-06 16:43:54 +0530

    Linear Search Practice

---
+++ Array/LinearSearch.java
@@ -0,0 +1,14 @@
+package Array;
+
+public class LinearSearch {
+   public static void main(String[] args) {
+    int arr[]={1,3,23,45,35,68,56,10,24,12,9};
+    int target=10;
+    for(int i=0;i<arr.length;i++){
+if(arr[i]==target){
+    System.out.println("Target is found at index : "+ i);
+  
+    }
+   } 
+   }
+}
