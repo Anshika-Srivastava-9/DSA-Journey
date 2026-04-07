7e57c4077f5f1bf126d4fcefe550510589127605
Anshika-Srivastava-9 <srianshika89@gmail.com>
2026-04-07 20:59:37 +0530

    Multiplication

---
+++ Method/multiply.java
@@ -0,0 +1,12 @@
+package Method;
+
+public class multiply {
+    public static int multiply(int a,int b){
+        int product=a*b;
+        return product;
+    }
+    public static void main(String[] args) {
+        int prod=multiply(2, 5);
+        System.out.println(prod);
+        }
+}
