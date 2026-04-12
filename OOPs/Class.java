f508c123320d566119523c99c5799a8e8977fd9f
Anshika-Srivastava-9 <srianshika89@gmail.com>
2026-04-12 23:56:22 +0530

    OOPs basics

---
+++ OOPs/Class.java
@@ -0,0 +1,14 @@
+package OOPs;
+public class Class {
+    
+}
+class Pen{
+    String color;
+    int tip;
+    void setColr(String newColor){
+        color=newColor;
+    }
+    void setTip(int newTip){
+        tip=newTip;
+        }
+}
