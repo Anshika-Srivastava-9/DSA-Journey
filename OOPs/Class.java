97d9e9626d43d3e0daa093ac852ff7546201ad4f
Anshika-Srivastava-9 <srianshika89@gmail.com>
2026-04-13 23:16:04 +0530

    More about classes

--- OOPs/Class.java
+++ OOPs/Class.java
@@ -1,6 +1,12 @@
 package OOPs;
 public class Class {
-    
+   public static void main(String[] args) {
+    Pen P1=new Pen();
+    P1.setColr("Blue");
+    P1.setTip(5);
+    System.out.println("The color of pen is " +P1.color +" And its tip size is "+ P1.tip);
+
+   } 
 }
 class Pen{
     String color;
@@ -12,3 +18,8 @@ class Pen{
         tip=newTip;
         }
 }
+
+class Animal{
+
+}
+ 
