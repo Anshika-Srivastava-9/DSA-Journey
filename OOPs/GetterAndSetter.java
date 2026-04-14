72dc9340448afe2e53812bf7bbea625b2176fd94
Anshika-Srivastava-9 <srianshika89@gmail.com>
2026-04-14 13:28:40 +0530

    Getter and setter

---
+++ OOPs/GetterAndSetter.java
@@ -0,0 +1,34 @@
+package OOPs;
+
+public class GetterAndSetter {
+    public static void main(String[] args) {
+        Pen p1 = new Pen();
+
+        p1.setColor("Red");
+        System.out.println(p1.getColor()); 
+
+        p1.setTip(2);
+        System.out.println(p1.getTip());
+    }
+}
+
+class Pen {
+    private String color;
+    private int tip;
+
+    String getColor() {
+        return this.color;
+    }
+
+    int getTip() {
+        return this.tip;
+    }
+
+    void setColor(String newColor) {
+        this.color = newColor;
+    }
+
+    void setTip(int tip) {
+        this.tip = tip;
+    }
+}
