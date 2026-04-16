0a56c407e08edb9ea255671237034059484f0041
Anshika-Srivastava-9 <srianshika89@gmail.com>
2026-04-16 18:43:14 +0530

    Hybrid Inheritence

---
+++ OOPs/HybridInheritence.java
@@ -0,0 +1,18 @@
+package OOPs;
+
+public class HybridInheritence {
+    public static void main(String[] args) {
+        
+    }
+}
+class Animal{
+    String color;
+    String name;
+    void eat(){
+        System.out.println("Eats");
+    }
+    void breath(){
+        System.out.println("Breathes");
+    }
+}
+
