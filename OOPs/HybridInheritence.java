acad14da547d9ae18e5c4b8fe5296ba2c6ffca37
Anshika-Srivastava-9 <srianshika89@gmail.com>
2026-04-16 19:02:08 +0530

    Hybrid Inheritence

--- OOPs/HybridInheritence.java
+++ OOPs/HybridInheritence.java
@@ -2,7 +2,13 @@ package OOPs;
 
 public class HybridInheritence {
     public static void main(String[] args) {
-        
+    Shark s=new Shark();
+    s.Swim();
+    s.breath();
+    
+    Human h=new Human();
+    h.Speak();
+    h.Blood();
     }
 }
 class Animal{
@@ -15,4 +21,32 @@ class Animal{
         System.out.println("Breathes");
     }
 }
+class Fish extends Animal{
+    void Swim(){
+        System.out.println("Swims in water ");
+    }
+    void fins(){
+        System.out.println("use fins to swim");
+    }
+}
+ 
+class Shark extends Fish{
+    void teeth(){
+        System.out.println("Have sharp teeth");
+    }
+}
 
+class Mammal extends Animal{
+  void legs(){
+    System.out.println("Have legs");
+  }
+  void Blood(){
+    System.out.println("Are warm blooded");
+  }
+}
+
+class Human extends Mammal{
+    void Speak(){
+        System.out.println("Have vocal cord");
+    } 
+}
