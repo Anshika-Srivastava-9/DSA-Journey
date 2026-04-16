54a1e27e4c42f91ff906af2a5b87912a062199ae
Anshika-Srivastava-9 <srianshika89@gmail.com>
2026-04-16 18:46:10 +0530

    Polymorphism :Method Overriding

---
+++ OOPs/Overriding.java
@@ -0,0 +1,20 @@
+package OOPs;
+
+public class Overriding {
+    public static void main(String[] args) {
+       Deer d=new Deer();
+       d.eat(); 
+    }
+}
+
+class Animal{
+    void eat(){
+        System.out.println("Eats Anything");
+    }
+}
+
+class Deer extends Animal{
+    void eat(){
+        System.out.println("Eats grass");
+    }
+}
